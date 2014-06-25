25a10439251dc819bc0ae8b6c6bd1e95aa79809c
mousterian <rivladi@gmail.com>
2014-06-24 19:16:12 -0700

    updated organizations/edit views

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,3 +1,15 @@
 module OrganizationsHelper
 
+    def wrap(content)
+        content = content.gsub(/\n/, '<br/>')
+        sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+    end
+
+    private
+        def wrap_long_string(text, max_width = 20)
+            zero_width_space = "&#8203;"
+            regex = /.{1,#{max_width}}/
+            (text.length < max_width) ? text: text.scan(regex).join(zero_width_space)
+        end
+
 end
